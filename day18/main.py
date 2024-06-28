5d1c9e43ac63ad8ebb92d36c3c1f8f87381a3046
ljourdain <luc.jourdain@kyndryl.com>
2024-06-28 17:09:36 +0200

    day18 - spirograph

--- day18/main.py
+++ day18/main.py
@@ -1,9 +1,59 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
+import random
+
 
 timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("red")
-timmy.pendown()
-timmy.forward(200)
+timmy.speed("fastest")
+colormode(255)
+timmy.hideturtle()
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
+for _ in range(0, 361, 5):
+    timmy.setheading(_)
+    timmy.pencolor(random_color())
+    timmy.circle(150)
+
+
+'''def draw_dashed_line(turtle: Turtle):
+    for nbr in range(0,10):
+        turtle.forward(10)
+        turtle.penup()
+        turtle.forward(10)
+        turtle.pendown()
+
+
+def draw_shape(turtle: Turtle, x: int):
+    angle = 360/x
+    for _ in range(x):
+        turtle.forward(100)
+        turtle.right(angle)
+
+timmy.hideturtle()
+timmy.pensize(10)
+timmy.speed("fastest")
+for _ in range(200):
+    random_color = colors[random.randint(0, len(colors)-1)]
+    random_angle = 90 * random.randint(0,3)
+    timmy.setheading(random_angle)
+    timmy.pencolor(random_color)
+    timmy.forward(50)'''
+
+
+
+
+
+
+
+
+
+
+
 screen = Screen()
 screen.exitonclick()
+
