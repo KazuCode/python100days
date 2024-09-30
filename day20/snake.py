e136124f03028e66556a3198895b72cfbc7b4749
ljourdain <luc.jourdain@kyndryl.com>
2024-09-30 17:06:23 +0200

    day20

---
+++ day20/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.snake=[]
+        self.create_snake()
+        self.head = self.snake[0]
+        
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.snake.append(new_segment)
+
+    def move_snake(self):
+        for seg_index in range(len(self.snake)-1, 0, -1):
+            new_x = self.snake[seg_index-1].xcor()
+            new_y = self.snake[seg_index-1].ycor()
+            self.snake[seg_index].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
